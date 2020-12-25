c8c468e305d2fa7cebb7a0f5a6a4ac0032ef8b86
sgloba <>
2020-12-25 14:22:33 +0300

    deploy added servervv

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/front-todo'));
+app.use(express.static(__dirname + '/dist/frontTODO'));
 
 app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/front-todo/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/frontTODO/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
